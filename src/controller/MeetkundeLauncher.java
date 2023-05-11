8348249f3a9d0a32e58a361fd5e2cd1640ce6909
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2023-05-11 14:52:13 +0200

    Opdracht 3.2

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.Cirkel;
+import model.Punt;
 
 /**
  * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
@@ -11,7 +12,7 @@ public class MeetkundeLauncher {
     public static void main(String[] args) {
         Cirkel[] mijnCirkelArray = new Cirkel[3];
 
-        mijnCirkelArray[0] = new Cirkel(3, 1, 4, "groen");
+        mijnCirkelArray[0] = new Cirkel(3, new Punt(1, 4), "groen");
         mijnCirkelArray[1] = new Cirkel();
         mijnCirkelArray[2] = new Cirkel(6);
 
@@ -20,6 +21,8 @@ public class MeetkundeLauncher {
         for (int cirkel = 0; cirkel < mijnCirkelArray.length; cirkel++) {
             System.out.println(mijnCirkelArray[cirkel].geefOppervlak());
             System.out.println(mijnCirkelArray[cirkel].geefOmtrek());
+            System.out.println(mijnCirkelArray[cirkel].getMiddelpunt().getxCoordinaat());
+            System.out.println(mijnCirkelArray[cirkel].getMiddelpunt().getyCoordinaat());
             System.out.println(mijnCirkelArray[cirkel].vertelOverGrootte());
             System.out.println();
         }
