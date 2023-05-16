60d3c387f42ce9b27ec836ff8a9d74174e88577a
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2023-05-16 13:53:49 +0200

    Opdracht 9.2 File IO

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -2,9 +2,13 @@ package controller;
 
 import model.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -15,25 +19,32 @@ import java.util.Scanner;
 public class MeetkundeLauncher {
 
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        boolean onjuisteInvoer = true;
+        ArrayList<Figuur> rechthoeken = new ArrayList<>();
+        File rechthoekenBestand = new File("resources/Rechthoek.csv");
 
-        while (onjuisteInvoer) {
+        try (Scanner invoer = new Scanner(rechthoekenBestand)) {
+            while (invoer.hasNextLine()) {
+                String[] elementenVanRechthoek = invoer.nextLine().split(",");
+                double lengte = Double.parseDouble(elementenVanRechthoek[0]);
+                double breedte = Double.parseDouble(elementenVanRechthoek[1]);
+                double xCoordinaat = Double.parseDouble(elementenVanRechthoek[2]);
+                double yCoordinaat = Double.parseDouble(elementenVanRechthoek[3]);
+                String kleur = elementenVanRechthoek[4];
 
-            try {
-                System.out.print("Geef een straal: ");
-                double straal = keyboard.nextDouble();
-                Cirkel ingevoerdeCirkel = new Cirkel(straal);
-                System.out.println(ingevoerdeCirkel);
-                onjuisteInvoer = false;
-            } catch (IllegalArgumentException fout) {
-                System.out.println(fout.getMessage());
-            } catch (InputMismatchException | IOException fout) {
-                System.out.println("Dat was geen getal, probeer het nog een keer");
-                keyboard.nextLine();
-            } finally {
-                System.out.println("Je invoer is op de juiste wijze afgehandeld.");
+                rechthoeken.add(new Rechthoek(lengte, breedte, new Punt(xCoordinaat, yCoordinaat), kleur));
             }
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("Het bestand is niet gevonden");
+        }
+
+        File uitvoerBestand = new File("resources/Rechthoeken.txt");
+        try (PrintWriter printWriter = new PrintWriter(uitvoerBestand)) {
+            for (Figuur figuur : rechthoeken) {
+                printWriter.println(figuur);
+                printWriter.println();
+            }
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("Het bestand kan niet worden aangemaakt.");
         }
     }
 
